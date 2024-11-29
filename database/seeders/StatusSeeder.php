955a9f20e3f4a86c7d43820288bf44da6fe30e22
Philemon <etoundelight1@gmail.com>
2024-11-29 09:52:38 +0100

    editing status seeder

--- database/seeders/StatusSeeder.php
+++ database/seeders/StatusSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class StatusSeeder extends Seeder
@@ -10,8 +11,24 @@ class StatusSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        $statuses = [
+            'Non traité',
+            'En cours',
+            'Résolu',
+            'Rejeté',
+        ];
+
+        foreach ($statuses as $status) {
+            // Vérifie si le libellé existe déjà
+            if (!DB::table('status')->where('nom_status', $status)->exists()) {
+                DB::table('status')->insert([
+                    'nom_status' => $status,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
     }
 }
